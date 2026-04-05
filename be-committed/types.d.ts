aec68b322a4e5b85f634fd5ea67c39cda07a1c22
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-05 06:05:13 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

---
+++ be-committed/types.d.ts
@@ -0,0 +1,23 @@
+export interface EndUserProps{
+    to: string;
+    nudges: boolean;
+    on: string;
+}
+
+export interface AllProps extends EndUserProps {
+
+}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export type BAP = AllProps & BEAllProps & RoundaboutReady;
+
+export interface Actions{
+    hydrate(self: BAP): ProPAP;
+    // findTarget(self: this): Promise<void>;
+    // handleCommit(self: this, e: KeyboardEvent): Promise<void>;
+}

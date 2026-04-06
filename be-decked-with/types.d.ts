8e2b0e4c7fef47a1164cc334b121d12b24a0fdc7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-06 18:27:59 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- be-decked-with/types.d.ts
+++ be-decked-with/types.d.ts
@@ -5,7 +5,9 @@ export interface EndUserProps{
 }
 
 export interface AllProps extends EndUserProps{
-    template: HTMLTemplateElement
+    template: HTMLTemplateElement;
+    enhancedElement: Element;
+    resolved: boolean;
 }
 
 export type AP = AllProps;
@@ -14,11 +16,11 @@ export type PAP = Partial<AP>;
 
 export type ProPAP = Promise<PAP>;
 
-export type BAP = AP & BEAllProps;
+//export type BAP = AP & BEAllProps;
 
 export interface Actions{
     act(self: BAP): PAP
     fetchRemoteTemplate(self: BAP): ProPAP
-    upShadowSearch(self: BAP): PAP
+    upShadowSearch(self: BAP): ProPAP
     init(self: BAP, initVals: PAP): Promise<void>
 }

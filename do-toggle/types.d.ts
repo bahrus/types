089536385c0c0fd04fa5dc100227ed76c43d137f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-19 18:53:25 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

---
+++ do-toggle/types.d.ts
@@ -0,0 +1,31 @@
+import { StatementsResult } from "../nested-regex-groups/types";
+
+export interface Specifier {
+    selector?: string;
+    prop?: string;
+}
+
+export interface EndUserProps{}
+
+export interface AllProps extends EndUserProps{
+    enhancedElement: Element;
+    parsedStatements: StatementsResult<TogglingParameters>;
+    resolved?: boolean;
+}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP  = Promise<PAP>
+
+export interface Actions{
+    init(self: AllProps, enhancedElement: Element, initVals: PAP): void;
+    hydrate(self: AP): ProPAP;
+    handleEvent(self: AP, e: Event, parsedStatement: TogglingParameters): void;
+}
+
+export interface TogglingParameters {
+    remoteSpecifier: Specifier;
+    localEventType?: string;
+}

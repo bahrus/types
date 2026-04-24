7c7d4f0b70f7007ce2aedfab808936baa3c24db0
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-04-23 20:45:10 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

---
+++ be-bound/types.d.ts
@@ -0,0 +1,60 @@
+import { Specifier } from "../trans-render/dss/types";
+import {AbsorbingObject, SharingObject} from '../trans-render/asmr/types';
+import { StatementsResult } from "../nested-regex-groups/types";
+
+export interface EndUserProps{
+    bindingRules?: StatementsResult<BindingRule>;
+}
+
+export interface AllProps extends EndUserProps{
+    enhancedElement: Element;
+    bindings: Array<Binding>,
+    isParsed?: boolean,
+    rawStatements?: Array<string>
+}
+
+export type SignalEnhancement = 'be-value-added' | 'be-propagating' | undefined;
+
+export interface BindingRule {
+    
+    localProp?: string,
+    localEvent?: string,
+    remoteSpecifierString?: string,
+    remoteSpecifier?: Specifier,
+
+
+}
+
+export interface Binding {
+    //new and improved
+    localAbsObj: AbsorbingObject;
+    localShareObj: SharingObject;
+    remoteAbsObj: AbsorbingObject;
+    remoteShareObj: SharingObject;
+    //remoteRef: WeakRef<Element>;
+}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+
+export interface Actions{
+    noAttrs(self: AP): ProPAP;
+    getBindings(self: AP): ProPAP;
+    hydrate(self: AP): ProPAP;
+    onRawStatements(self: AP): void;
+}
+
+export type WithStatement = string;
+
+export type BetweenStatement = string;
+
+export type TriggerSource = 'local' | 'remote' | 'tie';
+
+export interface SpecificityResult {
+    val?: any,
+    winner?: TriggerSource;
+}

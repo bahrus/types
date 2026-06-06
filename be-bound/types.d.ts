f51318b0cd630f3fc0d4f1b2aaf6f1200d79f10a
bahrus <anderson.bruce.b@gmail.com>
2026-06-06 08:48:40 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- be-bound/types.d.ts
+++ be-bound/types.d.ts
@@ -1,5 +1,6 @@
+import { ElementEnhancementGateway, SpawnContext } from "../assign-gingerly/types";
 import { Specifier } from "../trans-render/dss/types";
-import {AbsorbingObject, SharingObject} from '../trans-render/asmr/types';
+import { AbsorbingObject, SharingObject} from '../trans-render/asmr/types';
 import { StatementsResult } from "../nested-regex-groups/types";
 
 export interface EndUserProps{
@@ -43,6 +44,7 @@ export type ProPAP = Promise<PAP>;
 
 
 export interface Actions{
+    init(self: AllProps, enhancedElement: Element, ctx: SpawnContext, initVals: PAP): Promise<void>;
     noAttrs(self: AP): ProPAP;
     getBindings(self: AP): ProPAP;
     hydrate(self: AP): ProPAP;

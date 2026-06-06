517252a582ab9dd59c8b03d041e0929bbaf80cb9
bahrus <anderson.bruce.b@gmail.com>
2026-06-06 16:36:05 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- be-bound/types.d.ts
+++ be-bound/types.d.ts
@@ -1,15 +1,15 @@
 import { ElementEnhancementGateway, SpawnContext } from "../assign-gingerly/types";
-import { Specifier } from "../trans-render/dss/types";
-import { AbsorbingObject, SharingObject} from '../trans-render/asmr/types';
+//import { Specifier } from "../trans-render/dss/types";
+//import { AbsorbingObject, SharingObject} from '../trans-render/asmr/types';
 import { StatementsResult } from "../nested-regex-groups/types";
 
 export interface EndUserProps{
-    bindingRules?: StatementsResult<BindingRule>;
+    bindingRules: StatementsResult<BindingRule>;
 }
 
 export interface AllProps extends EndUserProps{
-    enhancedElement: Element;
-    bindings: Array<Binding>,
+    enhancedElement: Element & ElementEnhancementGateway;
+    //bindings: Array<Binding>,
     isParsed?: boolean,
     rawStatements?: Array<string>
 }
@@ -22,19 +22,19 @@ export interface BindingRule {
     localEvent?: string,
     remoteId?: string,
     remoteProp?: string,
-    remoteSpecifier?: Specifier,
+    //remoteSpecifier?: Specifier,
 
 
 }
 
-export interface Binding {
-    //new and improved
-    localAbsObj: AbsorbingObject;
-    localShareObj: SharingObject;
-    remoteAbsObj: AbsorbingObject;
-    remoteShareObj: SharingObject;
-    //remoteRef: WeakRef<Element>;
-}
+// export interface Binding {
+//     //new and improved
+//     localAbsObj: AbsorbingObject;
+//     localShareObj: SharingObject;
+//     remoteAbsObj: AbsorbingObject;
+//     remoteShareObj: SharingObject;
+//     //remoteRef: WeakRef<Element>;
+// }
 
 export type AP = AllProps;
 
@@ -45,7 +45,7 @@ export type ProPAP = Promise<PAP>;
 
 export interface Actions{
     init(self: AllProps, enhancedElement: Element, ctx: SpawnContext, initVals: PAP): Promise<void>;
-    noAttrs(self: AP): ProPAP;
+    //noAttrs(self: AP): ProPAP;
     getBindings(self: AP): ProPAP;
     hydrate(self: AP): ProPAP;
     onRawStatements(self: AP): void;

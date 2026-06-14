c8495f382ac089c80aeb8e027c13d8c95d4c43bd
bahrus <anderson.bruce.b@gmail.com>
2026-06-14 14:05:29 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

---
+++ be-observing/types.d.ts
@@ -0,0 +1,56 @@
+import { StatementsResult } from "../nested-regex-groups/types";
+import { ElementEnhancementGateway, SpawnContext } from "../assign-gingerly/types";
+
+export interface RemoteSpecifier {
+    id?: string;
+    prop?: string;
+    path?: string;
+    evtName?: string;
+    as?: 'number' | 'boolean' | 'string' | 'object' | 'regexp' | 'urlpattern' | 'boolean|number';
+    constVal?: any;
+    enhKey?: string;
+    ish?: boolean;
+    host?: boolean;
+    self?: boolean;
+}
+
+export interface ObservingParameters {
+    localPropToSet?: string;
+    remoteSpecifiers: Array<RemoteSpecifier>;
+    punt: boolean;
+    aggKey: string;
+    interpolatingExpr: string;
+    JSExpr: string;
+    ONExpr: string;
+    action?:
+        | 'set'
+        | 'toggle'
+        | 'increment'
+        | 'decrement'
+        | 'set-class'
+        | 'set-part';
+}
+
+export interface EndUserProps {
+    parsedStatements: StatementsResult<ObservingParameters>;
+}
+
+export interface AllProps extends EndUserProps {
+    enhancedElement: Element;
+    enhKey: string;
+    customHandlers: Map<string, any>;
+    ws: Array<any>;
+    didInferring: boolean;
+}
+
+export type AP = AllProps;
+
+export type PAP = Partial<AP>;
+
+export type ProPAP = Promise<PAP>;
+
+export interface Actions {
+    init(self: AP, enhancedElement: Element & ElementEnhancementGateway, ctx: SpawnContext, initVals: PAP): Promise<void>;
+    noAttrs(self: AP): ProPAP;
+    seek(self: AP): ProPAP;
+}

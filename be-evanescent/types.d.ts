2f72707fe66b37ad8329fa5da1baf34987210142
bahrus <anderson.bruce.b@gmail.com>
2026-06-18 18:07:57 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

---
+++ be-evanescent/types.d.ts
@@ -0,0 +1,19 @@
+import { ElementEnhancementGateway, SpawnContext } from "../assign-gingerly/types";
+
+export interface EndUserProps{
+    whenDefined: string[];
+}
+
+export interface AllProps extends EndUserProps{
+    enhancedElement: Element;
+    resolved: boolean;
+}
+
+export type AP = AllProps;
+export type PAP = Partial<AP>;
+export type ProPAP = Promise<PAP>;
+
+export interface Actions{
+    init(self: AP, enhancedElement: Element & ElementEnhancementGateway, ctx: SpawnContext, initVals: PAP): Promise<void>;
+    onWhenDefined(self: AP): ProPAP;
+}

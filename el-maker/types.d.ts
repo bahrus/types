3bb3a0e8d33ab5f76ef8d0f334477b256c4fd9f5
bahrus <anderson.bruce.b@gmail.com>
2026-08-07 17:46:31 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- el-maker/types.d.ts
+++ el-maker/types.d.ts
@@ -1,11 +1,18 @@
 import {RAConfig} from '../roundabout/types.js';
+import {FontFaceFeatureConfig} from '../font-face-feature/types.js';
 
 export interface ElMakerConfig<AllProps = any, TActions = AllProps> {
     assignFeatures: {
-        roundabout: {
+        roundabout?: {
             spawn?: string,
             customData: {
                 raConfig: RAConfig<AllProps, TActions, TActions>
+            }
+        },
+        fontMgr?: {
+            spawn?: string,
+            customData: {
+                fontFaceFeatureConfig: FontFaceConfig
             }
         }
     }

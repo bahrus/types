9ce7477a5009ccc80ffd39729a92732991ba096b
bahrus <anderson.bruce.b@gmail.com>
2026-08-07 17:39:19 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- el-maker/types.d.ts
+++ el-maker/types.d.ts
@@ -3,7 +3,7 @@ import {RAConfig} from '../roundabout/types.js';
 export interface ElMakerConfig<AllProps = any, TActions = AllProps> {
     assignFeatures: {
         roundabout: {
-            spawn: string,
+            spawn?: string,
             customData: {
                 raConfig: RAConfig<AllProps, TActions, TActions>
             }

84054dc44a8cd46f3f7ab71b6134ee6363b79bff
bahrus <anderson.bruce.b@gmail.com>
2026-08-17 17:44:21 -0400

    Merge branch 'baseline' of https://github.com/bahrus/types into baseline

--- el-maker/types.d.ts
+++ el-maker/types.d.ts
@@ -2,6 +2,7 @@ import {RAConfig} from '../roundabout/types.js';
 import {FontFaceFeatureConfig} from '../font-face-feature/types.js';
 import {CustomData as TSCD} from '../truth-sourcer/types.js';
 import {CustomData as FUCD} from '../face-up/types.js';
+import {AttrPatterns} from '../assign-gingerly/types.js';
 
 export interface ElMakerConfig<AllProps = any, TActions = AllProps> {
     assignFeatures: {
@@ -10,6 +11,7 @@ export interface ElMakerConfig<AllProps = any, TActions = AllProps> {
             customData: {
                 raConfig: RAConfig<AllProps, TActions, TActions>
             }
+            withAttrs: AttrPatterns<AllProps>
         },
         fontMgr?: {
             spawn?: string,
